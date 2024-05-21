e3cadeadf6dcac1beb7f5a3f771b9885959e2655
Nounourek <nounourek3@yahoo.fr>
2024-05-21 12:42:34 +0200

    Programa cubiertos completo

--- printCubierto.js
+++ printCubierto.js
@@ -1,13 +1,63 @@
-console.log("l  l  l")
-console.log("l l  l ")
-console.log("l l  l ")
-console.log("l l  l ")
-console.log("l l  l ")
-console.log("   l   ")
-console.log("   l   ")
-console.log("   l   ")
-console.log("   l   ")
-console.log("   l   ")
-console.log("   l   ")
+const sc = require("prompt-sync")()
+let num
 
-console.log("El profe estuvo aqui");
+do {
+num=sc("elige num: 1, 2, 3") 
+if (num ==1) {
+    console.log("Se imprime un tenedor");
+    pintarTenedor()
+    
+}else if (num ==2){
+    console.log("Se imprime un cuchillo");
+    pintarCuchillo()
+
+}else if(num==3){
+    console.log("Se imprime una cuchara");
+    pintarCuchara()
+    }     
+    
+}
+while (num <=3)
+    console.log("Error, elige 1, 2 o 3")
+
+
+function pintarTenedor() {
+    console.log("l l  l")
+    console.log("l l  l")
+    console.log("l l  l")
+    console.log("l_l _l")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l"   )    
+    
+}
+function pintarCuchillo() {
+    console.log("    l")
+    console.log("    l")
+    console.log("    l")
+    console.log("   l")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l   ")
+    console.log("  l"   )  
+}
+function pintarCuchara(){
+    console.log("l     l")
+    console.log("l     l")
+    console.log("l     l")
+    console.log(" l    l")
+    console.log("   l   ")
+    console.log("   l   ")
+    console.log("   l   ")
+    console.log("   l   ")
+    console.log("   l   ")
+    console.log("   l   ")
+    console.log("   l"   )  
+}
